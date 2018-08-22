f5e73b7acefe2b3a8a48180a53420375402644ca
Ken Snyder <ken@ken.net>
2018-08-21 17:57:40 -0700

    first commit after yo template

---
+++ scripts/get-secret.ts
@@ -0,0 +1,68 @@
+// tslint:disable:no-implicit-dependencies
+import chalk from "chalk";
+import { asyncExec } from "async-shelljs";
+import * as yaml from "js-yaml";
+import { parseArgv } from "./lib/util";
+import { IServerlessConfig } from "common-types";
+import { readFileSync } from "fs";
+import { gzip } from "zlib";
+import { promisify } from "util";
+import { listSecrets } from "./lib/aws-ssm";
+const gzipAsync = promisify<Buffer, Buffer>(gzip);
+
+const { params, options } = parseArgv()("--region", "--profile", "--key");
+const [param, value] = params;
+const config: IServerlessConfig = yaml.safeLoad(
+  readFileSync("./serverless.yml", { encoding: "utf-8" })
+);
+
+const profile = options.profile || config.provider.profile || "default";
+const region = options.region || config.provider.region || "us-east-1";
+
+(async () => {
+  try {
+    if (!param) {
+      console.log(
+        `You must state a variable name: ${chalk.grey("yarn get-secret [VAR]")}`
+      );
+      console.log();
+      console.log(`Variables in the "${profile}" AWS profile include:`);
+      const secrets = await listSecrets();
+      console.log();
+      console.log(secrets.map(i => `  > ${i.Name}`).join("\n"));
+      console.log();
+
+      process.exit();
+    }
+
+    console.log(
+      `Using the AWS ${chalk.bold.green(profile)} profile to get ${chalk.bold.green(
+        param
+      )} in the ${chalk.bold.green(region)} region${
+        options.compress ? " [ compressed ]" : ""
+      }.`
+    );
+    const key = options.key ? `--key ${options.key}` : "";
+    const valueToSet: string = options.decompress
+      ? ((await gzipAsync(Buffer.from(value))) as Buffer).toString("utf-8")
+      : value;
+    const command = `aws --profile ${profile} --region ${region} ssm get-parameters --names ${param} `;
+    console.log(chalk.grey.dim(`> ${command}`));
+
+    const results = await asyncExec(`${command}`, { silent: true });
+    console.log(
+      options.v || options.verbose ? results : JSON.parse(results).Parameters[0].Value
+    );
+  } catch (e) {
+    if (e.message.indexOf("ParameterAlreadyExists") !== -1) {
+      console.log(
+        `The parameter ${chalk.yellow.bold(params[0])} is ${chalk.italic(
+          "already set" + chalk.reset(".")
+        )} To overwrite the value you must use the "-o" or "--overwrite" flag.`
+      );
+      process.exit(0);
+    } else {
+      console.error(e.message);
+    }
+  }
+})();
---
+++ scripts/list-secrets.ts
@@ -0,0 +1,59 @@
+// tslint:disable:no-implicit-dependencies
+import chalk from "chalk";
+import { asyncExec } from "async-shelljs";
+import * as yaml from "js-yaml";
+import { parseArgv } from "./lib/util";
+import { IServerlessConfig, IDictionary } from "common-types";
+import { readFileSync } from "fs";
+
+const commandLine: string[] = process.argv.slice(2);
+const { params, options } = parseArgv("--json")("--region", "--profile");
+const config: IServerlessConfig = yaml.safeLoad(
+  readFileSync("./serverless.yml", { encoding: "utf-8" })
+);
+
+const profile = options.profile || config.provider.profile || "default";
+const type = options.type || "String";
+const region = options.region || config.provider.region || "us-east-1";
+
+console.log(
+  `Using the AWS ${chalk.bold.green(profile)} profile in the ${chalk.bold.green(
+    region
+  )} region to ${chalk.bold("list")} all parameters in ssm.`
+);
+
+(async () => {
+  try {
+    const command = `aws --profile ${profile} --region ${region} ssm describe-parameters`;
+    console.log(chalk.grey.dim(`> ${command}\n`));
+
+    const results: string = await asyncExec(command, { silent: true });
+    if (options.json) {
+      console.log(JSON.parse(results).Parameters);
+    } else {
+      JSON.parse(results).Parameters.map((param: IDictionary) => {
+        const datetime = new Date(
+          Math.round(param.LastModifiedDate * 1000)
+        ).toISOString();
+        const who = param.LastModifiedUser.split(":").pop();
+
+        console.log(
+          `${chalk.green.bold(param.Name)}: ${chalk.italic.grey(
+            param.Description
+          )}${chalk.reset(" ")}`
+        );
+
+        console.log(`  - v${param.Version} of type ${chalk.yellow.bold(param.Type)}`);
+        console.log(
+          `  - Last modified on ${chalk.bold.yellow(datetime)} by ${chalk.yellow.bold(
+            who
+          )}`
+        );
+      });
+      console.log();
+    }
+  } catch (e) {
+    console.error(e.message);
+    process.exit();
+  }
+})();
