0201a71a65fa5a5cf2e0db1949595abe62ecb82e
Ken Snyder <ken@ken.net>
2018-08-22 22:55:53 -0700

    listen bot sends to discord static text

--- src/handlers/listen.ts
+++ src/handlers/listen.ts
@@ -8,6 +8,7 @@ import {
   IDictionary
 } from "common-types";
 import axios from "axios";
+import * as path from "path";
 
 export async function handler(
   event: IAWSLambdaProxyIntegrationRequest,
@@ -20,7 +21,33 @@ export async function handler(
   const correlationId = (event.headers as IDictionary)["X-Request-UUID"];
   console.log(`changeEvent: ${changeEvent}; correlationId: ${correlationId}`);
   console.log("payload\n", JSON.stringify(payload, null, 2));
-  // await axios.put(`https://discordapp.com/api/webhooks/`)
+  console.log("context\n", JSON.stringify(context, null, 2));
+  process.env.repo =
+    "481570289558355969/KKKrc3eUn5mIGvUDVz4_UKqx5tQOHhMAroReVBLXGsDtPD-PmefsNkoX6sm92nJASWa0";
+  const discordWebhookUrl = path.join(
+    `https://discordapp.com/api/webhooks/`,
+    process.env.repo
+  );
+  console.log("URL", discordWebhookUrl);
+
+  try {
+    await axios({
+      method: "post",
+      url: process.env.repo,
+      baseURL: `https://discordapp.com/api/webhooks/`,
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": "DiscordBot"
+      },
+      data: {
+        content: "hello from listen bot"
+      }
+    });
+  } catch (e) {
+    console.error(e);
+    callback(e, { statusCode: e.statusCode });
+    process.exit();
+  }
 
   callback(null, {
     statusCode: AWSGatewayStatusCode.Success,
